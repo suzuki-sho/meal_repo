ae1af66c0fd34439c7cafaf1e59088ac2760cf4c
suzuki-sho <mrmyself.chun1000ten@gmail.com>
2020-10-26 14:37:48 +0900

    コントローラー設定＆リンク

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "reports#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :reports, only: [:index, :create]
+  end
 end
