73ba1947ee002da5d2fa418ea67e5e585e9775d0
suzuki-sho <mrmyself.chun1000ten@gmail.com>
2020-10-26 19:01:46 +0900

    reportコントローラー修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,16 +5,24 @@ class ReportsController < ApplicationController
 
   def new
     @report = Report.new
-    @room = Room.find_by(id:1)
+    @room = Room.find(params[:room_id])
+    @reports = @room.reports.includes(:user)
   end
 
   def create
-    Report.create(report_params)
+    @room = Room.find(params[:room_id])
+    @report = @room.reports.new(report_params)
+    if @reports.save
+      redirect_to room_reports_path(@room)
+    else
+      @reports = @room.reports.includes(:user)
+      render :index
+    end
   end
 
   private
   def report_params
-    params.require(:report).permit(:body, :image)
+    params.require(:report).permit(:body, :image).merge(user_id: current_user.id)
   end
 
 end
