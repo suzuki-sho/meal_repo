df105f03ff97babc62d511eb8d819afda8732831
suzuki-sho <mrmyself.chun1000ten@gmail.com>
2020-10-29 15:09:15 +0900

    readモデル削除

--- app/models/read.rb
+++
@@ -1,2 +0,0 @@
-class Read < ApplicationRecord
-end
--- db/migrate/20201029021753_create_reads.rb
+++
@@ -1,8 +0,0 @@
-class CreateReads < ActiveRecord::Migration[6.0]
-  def change
-    create_table :reads do |t|
-
-      t.timestamps
-    end
-  end
-end
