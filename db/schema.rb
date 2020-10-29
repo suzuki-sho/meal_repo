1b36a8cc0cbe735d5d2ccadb3122b320258a6b91
suzuki-sho <mrmyself.chun1000ten@gmail.com>
2020-10-29 15:22:36 +0900

    likedカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_27_032819) do
+ActiveRecord::Schema.define(version: 2020_10_29_062045) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_10_27_032819) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "liked"
     t.index ["room_id"], name: "index_reports_on_room_id"
     t.index ["user_id"], name: "index_reports_on_user_id"
   end
