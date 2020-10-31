df49a09b02bc82410e724c1ee6cde17e4a75a784
suzuki-sho <mrmyself.chun1000ten@gmail.com>
2020-10-31 17:00:37 +0900

    プレビュー条件

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,15 +1,17 @@
-document.addEventListener('DOMContentLoaded', function(){
-  const ImageList = document.getElementById('image-list');
-  document.getElementById('report_picture').addEventListener('change', function(e){
-    const file = e.target.files[0];
-    const blob = window.URL.createObjectURL(file);
+if (document.URL.match( /new/ )) {
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('image-list');
+    document.getElementById('report_picture').addEventListener('change', function(e){
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
 
-    const imageElement = document.createElement('div');
-    const blobImage = document.createElement('img');
-    blobImage.setAttribute('src', blob);
-    blobImage.setAttribute('class', "preview-image")
+      const imageElement = document.createElement('div');
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+      blobImage.setAttribute('class', "preview-image")
 
-    imageElement.appendChild(blobImage);
-    ImageList.appendChild(imageElement);
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    });
   });
-});
+}
