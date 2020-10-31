7726c8d36080d37c7bb1e7f23e6d50df779eaf14
suzuki-sho <mrmyself.chun1000ten@gmail.com>
2020-10-31 15:55:25 +0900

    HTML要素生成

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -2,5 +2,10 @@ document.addEventListener('DOMContentLoaded', function(){
   const ImageList = document.getElementById('image-list');
   document.getElementById('report_picture').addEventListener('change', function(e){
     const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    const imageElement = document.createElement('div');
+    const blobImage = document.createElement('img');
+    blobImage.setAttribute('src', blob);
   });
 });
