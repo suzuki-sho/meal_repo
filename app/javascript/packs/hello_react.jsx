2de3f464198951278dae5a64fef9f8651f488f24
suzuki-sho <mrmyself.chun1000ten@gmail.com>
2020-11-08 14:02:32 +0900

    フッター修正

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -6,14 +6,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
+
+
+
 const Hello = (props) => {
   return (
   <div className='info-wrapper'>
     <div className='info'>
       <div className='copy-container'>
-        <h1>製作者</h1>
+        <h1>お問い合わせ</h1>
         {props.title}
-        <h2>お問い合わせ</h2>
       </div>
     </div>
   </div>
@@ -21,7 +23,7 @@ const Hello = (props) => {
 };
 
 Hello.defaultProps = {
-  title: 'David'
+  title: 'https://twitter.com/syo_suzuki_'
 }
 
 Hello.propTypes = {
@@ -34,4 +36,3 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.appendChild(document.createElement('div')),
   )
 })
-
